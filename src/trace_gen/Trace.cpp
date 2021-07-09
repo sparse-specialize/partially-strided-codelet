25a522aa3ed44a222c9a4432eda3274b015110ae
Kazem <kazem.cheshmi@gmail.com>
2021-07-09 17:55:16 -0400

    a simple trace-gen for spmv is added

---
+++ src/trace_gen/Trace.cpp
@@ -0,0 +1,56 @@
+//
+// Created by kazem on 7/8/21.
+//
+
+#include <iostream>
+#include "Trace.h"
+
+namespace sparse_avx{
+
+ Trace::Trace(int n):_num_trace(n) {
+  _mem_addr = new int[3*n]();
+  _op_codes = new int[n]();
+  //_tuple = new Tuple*[n];
+  _trace_threshold = 1e5;
+  _pre_alloc = false;
+ }
+
+ Trace::Trace(int n, int *ma, int *oc, int np):_num_trace(n),_mem_addr(ma),
+ _op_codes(oc), _num_partitions(np){
+  _pre_alloc = true;
+ }
+
+ Trace::~Trace() {
+  if(!_pre_alloc){
+   delete []_mem_addr;
+   delete []_op_codes;
+  }
+/*
+  for (int i = 0; i < _num_trace; ++i) {
+   delete _tuple[i];
+  }
+*/
+ }
+
+
+ int* Trace::MemAddr(){
+  return _mem_addr;
+ }
+
+ void Trace::print() {
+  for (int i = 0; i < _num_trace; ++i) {
+   std::cout<<_mem_addr[i*3]<<","<<_mem_addr[i*3+1]<<","<<_mem_addr[i*3+2]<<"\n";
+  }
+ }
+
+ void free_trace_list(Trace** list){
+  int list_size = list[0]->_num_partitions;
+  delete []list[0]->_op_codes;
+  delete []list[0]->_mem_addr;
+  for (int i = 0; i < list_size; ++i) {
+   delete list[i];
+  }
+  delete []list;
+ }
+
+}
