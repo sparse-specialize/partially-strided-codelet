3ed82f0bf89971f31de2add365f38b25f09aff2b
Kazem <kazem.cheshmi@gmail.com>
2021-07-12 14:44:12 -0400

    WIP: generic SpMV

--- src/SpMVGenericCode.cpp
+++ src/SpMVGenericCode.cpp
@@ -2,8 +2,15 @@
 // Created by kazem on 7/12/21.
 //
 
+#include <vector>
 #include "SpMVGenericCode.h"
+#include "Inspector.h"
 
 namespace DDT{
+
+ void spmv_generic(const int n, const int *Ap, const int *Ai, const double
+ *Ax, const double *x, double *y, const std::vector<Codelet*>& lst){
+
+ }
 
 }
